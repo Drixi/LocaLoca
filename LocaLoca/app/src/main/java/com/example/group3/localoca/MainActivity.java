7b534e5f21d6a5b89c53ea787c549a7401316bf9
Sbearlin <abang13@student.aau.dk>
2014-10-22 11:03:21 +0200

    Login screen main window
    Added login screen, made it the main window upon start

--- LocaLoca/app/src/main/java/com/example/group3/localoca/MainActivity.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/MainActivity.java
@@ -4,15 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+    private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginBtn = (Button)findViewById(R.id.button);
+
     }
+
 
 
     @Override
@@ -33,4 +37,8 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private void btnPressed() {
+
+    }
+
 }
