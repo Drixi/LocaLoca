bc1fcecffe07cc87000a09c7ff387162e998c693
Sbearlin <abang13@student.aau.dk>
2014-10-28 13:52:48 +0100

    btn_main1 goes to old main menu

--- LocaLoca/app/src/main/java/com/example/group3/localoca/ITE_MainMenu.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/ITE_MainMenu.java
@@ -1,6 +1,7 @@
 package com.example.group3.localoca;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,7 +33,8 @@ public class ITE_MainMenu extends Activity {
         btn_main_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // CONTENT HERE //
+                Intent switchtoregister = new Intent(v.getContext(), MainMenu.class);
+                startActivity(switchtoregister);
             }
         });
     // Bottom right button
