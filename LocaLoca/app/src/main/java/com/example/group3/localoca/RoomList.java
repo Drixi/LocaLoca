a988ffd423d7b9a034af483c3019b5835301fcf4
Sbearlin <abang13@student.aau.dk>
2014-11-06 13:15:12 +0100

    test af fix

--- LocaLoca/app/src/main/java/com/example/group3/localoca/RoomList.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/RoomList.java
@@ -34,11 +34,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-<<<<<<< HEAD
-=======
 import java.io.IOException;
->>>>>>> origin/master
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
@@ -69,10 +65,7 @@ public class RoomList extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roomlist);
-<<<<<<< HEAD
-=======
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
->>>>>>> origin/master
         lvFloors = (ListView)findViewById(R.id.lvBuildings);
         img = (ImageView)findViewById(R.id.imgVFace);
         tvTest = (TextView)findViewById(R.id.tvTest);
