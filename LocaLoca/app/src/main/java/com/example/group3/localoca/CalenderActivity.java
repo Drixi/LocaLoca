53bdee2447bd6003d840deeca773767dbc3752f7
Sbearlin <abang13@student.aau.dk>
2014-11-06 15:14:05 +0100

    Calender uppgrade

--- LocaLoca/app/src/main/java/com/example/group3/localoca/CalenderActivity.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/CalenderActivity.java
@@ -3,9 +3,7 @@ package com.example.group3.localoca;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.format.Time;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -16,21 +14,31 @@ import java.util.Calendar;
  */
 public class CalenderActivity extends Activity {
 
-    TextView tvTest, tvTest2;
+    TextView tvDay1, tvDay2, tvDay3, tvDay4, tvDay5, tvDay6, tvDay7;
     SharedPreferences userinfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
-        tvTest = (TextView)findViewById(R.id.tvTest);
-        tvTest2 = (TextView)findViewById(R.id.tvTest2);
+        tvDay1 = (TextView)findViewById(R.id.tvDay1);
+        tvDay2 = (TextView)findViewById(R.id.tvDay2);
+        tvDay3 = (TextView)findViewById(R.id.tvDay3);
+        tvDay4 = (TextView)findViewById(R.id.tvDay4);
+        tvDay5 = (TextView)findViewById(R.id.tvDay5);
+        tvDay6 = (TextView)findViewById(R.id.tvDay6);
+        tvDay7 = (TextView)findViewById(R.id.tvDay7);
         userinfo = getSharedPreferences("userinfo", MODE_PRIVATE);
         String userid = userinfo.getString("userID", "");
         String test = userinfo.getString("userName", "");
-        //tvTest.setText("User ID = " + userid + " name = " + test);
-        tvTest.setText(getDateString(0));
-        tvTest2.setText(getDateString(1) + " " + getDateString(2));
+        //tvDay1.setText("User ID = " + userid + " name = " + test);
+        tvDay1.setText(getDateString(0));
+        tvDay2.setText(getDateString(1));
+        tvDay3.setText(getDateString(2));
+        tvDay4.setText(getDateString(3));
+        tvDay5.setText(getDateString(4));
+        tvDay6.setText(getDateString(5));
+        tvDay7.setText(getDateString(6));
     }
 
     private String getDateString(int days) {
--- LocaLoca/app/src/main/java/com/example/group3/localoca/RoomList.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/RoomList.java
@@ -5,22 +5,17 @@ import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -34,13 +29,11 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.io.IOException;
-import org.w3c.dom.Text;
+
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 
 public class RoomList extends Activity {
@@ -69,7 +62,7 @@ public class RoomList extends Activity {
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         lvFloors = (ListView)findViewById(R.id.lvBuildings);
         img = (ImageView)findViewById(R.id.imgVFace);
-        tvTest = (TextView)findViewById(R.id.tvTest);
+        tvTest = (TextView)findViewById(R.id.tvDay1);
         DownloadImage = (Button)findViewById(R.id.btnGetImage);
         btnCheckLocation = (Button)findViewById(R.id.btnLocationCheck);
         DownloadImage.setOnClickListener(new View.OnClickListener() {
