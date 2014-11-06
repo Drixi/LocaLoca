fa53f4d573d5d02003fbe5ea92498014d0dd74be
Sbearlin <abang13@student.aau.dk>
2014-11-06 13:04:08 +0100

    Logo and login info saved

--- LocaLoca/app/src/main/java/com/example/group3/localoca/CalenderActivity.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/CalenderActivity.java
@@ -1,21 +1,44 @@
 package com.example.group3.localoca;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by Asbjørn on 03-11-2014.
  */
 public class CalenderActivity extends Activity {
 
-    globalvalues globalvalues = new globalvalues();
-    String[] loginInfo = globalvalues.getLoginInfo();
+    TextView tvTest, tvTest2;
+    SharedPreferences userinfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
-        Toast.makeText(getBaseContext(), loginInfo[1], Toast.LENGTH_SHORT);
+        tvTest = (TextView)findViewById(R.id.tvTest);
+        tvTest2 = (TextView)findViewById(R.id.tvTest2);
+        userinfo = getSharedPreferences("userinfo", MODE_PRIVATE);
+        String userid = userinfo.getString("userID", "");
+        String test = userinfo.getString("userName", "");
+        //tvTest.setText("User ID = " + userid + " name = " + test);
+        tvTest.setText(getDateString(0));
+        tvTest2.setText(getDateString(1) + " " + getDateString(2));
+    }
+
+    private String getDateString(int days) {
+        DateFormat dateFormat = new SimpleDateFormat("EEEE - dd/MM/yyyy");
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_WEEK, days);
+        cal.add(Calendar.DATE, days);
+        return dateFormat.format(cal.getTime());
     }
 
 }
