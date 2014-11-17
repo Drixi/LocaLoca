b6d33c7f146aa6493374afde51a052c130f461ad
Drixi <michael91jensen@hotmail.com>
2014-11-17 22:19:03 +0100

    Last change before customization

--- LocaLoca/app/src/main/java/com/example/group3/localoca/DrawerActivity.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/DrawerActivity.java
@@ -1,5 +1,6 @@
 package com.example.group3.localoca;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -62,6 +63,12 @@ public class DrawerActivity extends ActionBarActivity implements AdapterView.OnI
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         drawerListener.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerListener.onConfigurationChanged(newConfig);
     }
 
     @Override
