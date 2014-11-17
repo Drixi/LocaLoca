7f129f0c345eca1a76a59f5fc63598aa24768df3
Michael Jensen <michael91jensen@hotmail.com>
2014-11-17 14:27:44 +0100

    more drawerwork

--- LocaLoca/app/src/main/java/com/example/group3/localoca/DrawerActivity.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/DrawerActivity.java
@@ -3,21 +3,38 @@ package com.example.group3.localoca;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by michael on 17-11-2014.
  */
-public class DrawerActivity extends ActionBarActivity{
+public class DrawerActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
 
     private DrawerLayout drawerLayout;
-    private ListView drawerList;
+    private ListView listView;
+    private String[] drawerObjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer);
         drawerLayout=(DrawerLayout) findViewById(R.id.drawerlayout);
-        drawerList=(ListView) findViewById(R.id.drawerList);
+        drawerObjects=getResources().getStringArray(R.array.drawerObjets);
+        listView =(ListView) findViewById(R.id.drawerList);
+        listView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1));
+        listView.setOnItemClickListener(this);
+
     }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+        long id ) {
+        Toast.makeText(this, drawerObjects[position]+" was selected ", Toast.LENGTH_LONG).show();
+    }
+
+
+    
 }
