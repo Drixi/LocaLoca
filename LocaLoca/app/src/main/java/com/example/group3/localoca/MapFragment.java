62c9cd668d6972b9b424fe39286bbf2135ff3c92
Drixi <michael91jensen@hotmail.com>
2015-01-21 16:22:17 +0100

    Zoom enabled in portrait mode
    crashes in landscape mode

--- LocaLoca/app/src/main/java/com/example/group3/localoca/MapFragment.java
+++ LocaLoca/app/src/main/java/com/example/group3/localoca/MapFragment.java
@@ -1,19 +1,27 @@
 package com.example.group3.localoca;
 
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class MapFragment extends Fragment {
+
+    ImageView img_first_floor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_blueprint, container, false);
+
+        img_first_floor = (ImageView) rootView.findViewById(R.id.imageEnhance);
+        new PhotoViewAttacher(img_first_floor);
 
         return rootView;
     }
-
 }
+
